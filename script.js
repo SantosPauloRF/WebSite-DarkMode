114fa798ea4e11ae7002690aecbfa78a36840a7f
SantosPauloRF <103541522+SantosPauloRF@users.noreply.github.com>
2022-05-29 15:55:02 -0300

    Md english

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 /*Apenas o script para animar o botão do dark mode e mudar as classes */
 
 let btn = document.querySelector(".btnDark");
-let circulo = document.querySelector(".circulo")
+let circulo = document.querySelector(".circulo");
+
 btn.addEventListener('click', function(){
 
-    document.body.classList.toggle("dark")
-    circulo.classList.toggle("circulo2")
+    document.body.classList.toggle("dark");
+    circulo.classList.toggle("circulo2");
 
 });
