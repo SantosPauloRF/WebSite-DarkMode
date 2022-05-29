66f4aaeedb25c666c1c3e4991cf327bd57ee12cd
SantosPauloRF <103541522+SantosPauloRF@users.noreply.github.com>
2022-05-29 14:19:12 -0300

    Melhorando a indentação

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*Apenas o script para animar o botão do dark mode e mudar as classes */
+
 let btn = document.querySelector(".btnDark");
 let circulo = document.querySelector(".circulo")
 btn.addEventListener('click', function(){
